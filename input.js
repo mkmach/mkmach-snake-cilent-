d1319e7ce790ca9ba489eb09732240aa0ad528f7
mkmach <mkmach@hotmail.com>
2022-12-11 15:42:32 -0800

    WASD movement input

--- input.js
+++ input.js
@@ -1,9 +1,12 @@
-
+let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const { connect } = require("./client");
+
+const setupInput = function (conn) {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
@@ -17,7 +20,24 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
     }
-  };
+  
+  //binding keys
+  if (key === 'w') {
+    connection.write("Move: up");
+    }
+
+  if (key === 'a') {
+    connection.write("Move: left");
+    }    
+
+  if (key === 's') {
+    connection.write("Move: down");
+    }
+
+  if (key === 'd') {
+    connection.write("Move: right");
+    }
+};
 
 module.exports = {
   setupInput,
